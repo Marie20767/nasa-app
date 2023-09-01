9f8d885f56536e2f57e8181a30af346246f1f0af
Marie20767 <marie20767@gmail.com>
2023-09-01 16:32:40 +0200

    Initial front end logic and UI

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,26 @@
+const getPlanets = async () => {
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
+}
+
+const getLaunches = async () => {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+const submitLaunch = async (launch) => {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+const abortLaunch = async (id) => {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  getPlanets,
+  getLaunches,
+  submitLaunch,
+  abortLaunch,
+};
